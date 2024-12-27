3d7af7589649417ef8b093a4bf7d61c76059a137
JPAUL <142168122+Jack-Pots@users.noreply.github.com>
2024-12-27 11:10:57 +0000

    UI Tweaks: Repurpose Two Button in home page.

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -51,16 +51,9 @@ export default withMermaid({
     ],
     socialLinks: [
       {   
-        icon: { svg: '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z"/></svg>' }, 
-          link: 'https://github.com/bca-students/content',
-          ariaLabel: 'Content Source'
-      },
-      { 
-        icon: { svg: '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M320-240 80-480l240-240 57 57-184 184 183 183-56 56Zm320 0-57-57 184-184-183-183 56-56 240 240-240 240Z"/></svg>' }, 
-        link: 'https://github.com/bca-students/bca-students.github.io',
-        ariaLabel: 'Site Source Code'
-      },
-      { icon: 'github', link: 'https://github.com/bca-students/', ariaLabel: 'Our Git Profile' }
+        icon: 'github', 
+        link: 'https://github.com/bca-students/', 
+        ariaLabel: 'Our Git Profile' }
     ]
   },
   mermaid: {
